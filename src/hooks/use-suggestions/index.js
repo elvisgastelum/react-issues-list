802c7b8cae54fbc56ed50bbd97a62ea670fef2ba
Elvis Gastelum <elvisgastelum@outlook.com>
2020-07-25 15:55:36 -0700

    created useGithubIssuesList hook

--- src/hooks/use-suggestions/index.js
+++ src/hooks/use-suggestions/index.js
@@ -1,20 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
+import { useGithubIssuesList } from '../use-github-issues-list'
 
 export const useSuggestions = (initialState) => {
+  const list = useGithubIssuesList([]);
+  const [searchResults, setSearchResults] = useState(initialState);
 
-  const [list, setList] = useState([])
-  const [searchResults, setSearchResults] = useState(initialState)
 
-  useEffect(() => {
-    async function fetchList() {
-      const data = await fetch("https://api.github.com/repos/facebook/react/issues?per_page=100");
-      const list = await data.json()
-
-      setList(list);
-    }
-    fetchList();
-  }, [])
 
   const newSearchResults = (e) => {
     setSearchResults(
@@ -22,33 +14,33 @@ export const useSuggestions = (initialState) => {
     )
   }
 
-  function searchIssues(search, itemList){
-    if(search === '' || itemList === [])
-      return [];
-
-
-    var rx = new RegExp(`([^"]*${search}[^"]*)`,'gi');
-    const listFiltered = itemList.filter(value => filterResults(value, rx))
-    const slicedList = listFiltered.slice(0, 5)
-    return slicedList;
-  }
-
-  function filterResults(value, rx){
-    return matchResult(value.title, rx) || matchResult(value.labels, rx) || matchResult(value.body, rx)
-  }
-
-  function matchResult(value, rx){
-    if (Array.isArray(value)) {
-      var result = value.filter(label => {
-        return String(label.name).match(rx)
-      })
-
-      return result.length > 0 ? true : false
-    }
-
-    return String(value).match(rx)
-  }
-
-
   return [ searchResults, newSearchResults ]
 };
+
+
+function searchIssues(search, itemList){
+  if(search === '' || itemList === [])
+    return [];
+
+
+  var rx = new RegExp(`([^"]*${search}[^"]*)`,'gi');
+  const listFiltered = itemList.filter(value => filterResults(value, rx))
+  const slicedList = listFiltered.slice(0, 5)
+  return slicedList;
+}
+
+function filterResults(value, rx){
+  return matchResult(value.title, rx) || matchResult(value.labels, rx) || matchResult(value.body, rx)
+}
+
+function matchResult(value, rx){
+  if (Array.isArray(value)) {
+    var result = value.filter(label => {
+      return String(label.name).match(rx)
+    })
+
+    return result.length > 0 ? true : false
+  }
+
+  return String(value).match(rx)
+}
